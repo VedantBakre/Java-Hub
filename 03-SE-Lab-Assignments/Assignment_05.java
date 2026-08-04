f210660ee368044f745c3a252575b3931b6676a7
Vedant Bakre <232570915+VedantBakre@users.noreply.github.com>
2026-08-04 11:15:52 +0530

    Add Staff Member class with polymorphism example
    Implemented runtime polymorphism with method overriding in a Staff Member class and its subclasses: Professor, Administrator, and Librarian.

---
+++ 03-SE-Lab-Assignments/Assignment_05.java
@@ -0,0 +1,4 @@
+/*Demonstrate run time polymorphism using method overriding, 
+develop a Staff Member with basic details like name and department, 
+inherit it into sub classes like professor , administrator and librarian 
+each with their specific duties and overriding methods, and display their roles. */
