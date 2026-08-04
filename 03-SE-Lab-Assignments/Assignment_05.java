a8c248a6a2fa694e15f724d8eab99c5b1298fa4c
Vedant Bakre <232570915+VedantBakre@users.noreply.github.com>
2026-08-04 11:16:29 +0530

    Add comment for runtime polymorphism demonstration
    Added a comment outlining the purpose of the class and its intended functionality.

--- 03-SE-Lab-Assignments/Assignment_05.java
+++ 03-SE-Lab-Assignments/Assignment_05.java
@@ -2,3 +2,5 @@
 develop a Staff Member with basic details like name and department, 
 inherit it into sub classes like professor , administrator and librarian 
 each with their specific duties and overriding methods, and display their roles. */
+
+// Coming Soon
